ed9d0cb4e9351a8f1f47ed00ef422fe316b1b98c
Larissa Freitas <freitaslarissa@id.uff.br>
2022-02-01 18:18:14 -0300

    First commit

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Student;
+
+//-------------------API RESTful CRUD simples usando o Laravel
+
+class ApiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function getAllStudents()
+    {
+        $students = Student::get()->toJson(JSON_PRETTY_PRINT);
+        return response($students, 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function createStudent(Request $request)
+    {
+        $student = new Student;
+        $student->name = $request->name;
+        $student->course = $request->course;
+        $student->save();
+
+        return response()->json([
+            "message" => "student record created"
+        ], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function getStudent($id)
+    {
+        if (Student::where('id', $id)->exists()) {
+            $student = Student::where('id', $id)->get()->toJson(JSON_PRETTY_PRINT);
+            return response($student, 200);
+          } else {
+            return response()->json([
+              "message" => "Student not found"
+            ], 404);
+          }
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function updateStudent(Request $request, $id)
+    {
+        if (Student::where('id', $id)->exists()) {
+            $student = Student::find($id);
+            $student->name = is_null($request->name) ? $student->name : $request->name;
+            $student->course = is_null($request->course) ? $student->course : $request->course;
+            $student->save();
+    
+            return response()->json([
+                "message" => "records updated successfully"
+            ], 200);
+            } else {
+            return response()->json([
+                "message" => "Student not found"
+            ], 404);
+            
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function deleteStudent($id)
+    {
+        if(Student::where('id', $id)->exists()) {
+            $student = Student::find($id);
+            $student->delete();
+    
+            return response()->json([
+              "message" => "records deleted"
+            ], 202);
+          } else {
+            return response()->json([
+              "message" => "Student not found"
+            ], 404);
+            }
+          
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ApiController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+
+//------------API RESTful CRUD simple using Laravel
+Route::get('students',[ApiController::class, 'getAllStudents']);
+Route::post('students',[ApiController::class, 'createStudent']);
+Route::get('students/{id}', [ApiController::class, 'getStudent']);
+Route::put('students/{id}',[ApiController::class, ' updateStudent']);
+Route::delete('students/{id}',[ApiController::class, 'deleteStudent']);
